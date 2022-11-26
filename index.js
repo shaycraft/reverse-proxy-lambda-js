a5440c3413f2b22c8802dceacd3b4443f0fc5c98
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-11-25 19:49:09 -0700

    feat: beginning skeleton structure

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
+/***
+ *
+ * @param event {
+ * { rawQueryString: string, httpMethod: string }
+ * }
+ * @returns {Promise<{body: string, statusCode: number}>}
+ */
 exports.handler = async (event) => {
 
-    const url = `https://sampleserver6.arcgisonline.com/${event['pathParameters']['proxy']}`;
+    let url = `https://sampleserver6.arcgisonline.com/${event['pathParameters']['proxy']}`;
+    let httpMethod;
 
     console.log('url will be ', url);
 
+    if (event.rawQueryString) {
+        url = `${url}?${event['rawQueryString']}`;
+    }
+
+    if (event.httpMethod) {
+        httpMethod = event.httpMethod;
+    } else {
+        httpMethod = event['requestContext']['http']['method'];
+    }
+
+    let headers = '';
+    if (event['headers']) {
+        headers = event['headers'];
+    }
+
+    delete headers['Host'];
+    delete headers['host'];
+
+    let body = event['body'] || null;
+
+
+
+
     // TODO implement
     const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        statusCode: 200, body: JSON.stringify('Hello from Lambda!'),
     };
     return response;
 };
