795fa9a0e1a760d9741e5b242672a3c275a9aae1
Sam Haycraft <phreakdawg@gmail.com>
2022-11-26 02:43:41 -0700

    chore:  implements prettier

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {ResponseStream, ResponseStreamType} from './response-stream';
+import { ResponseStream, ResponseStreamType } from './response-stream';
 import * as https from 'https';
 
 /***
@@ -8,45 +8,46 @@ import * as https from 'https';
  * @returns {Promise<{statusCode: number, body: string}>}
  */
 async function issueRequest(url, headers) {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    /**
+     *
+     * @type {RequestOptions}
+     */
+    const options = { headers };
 
-        /**
-         *
-         * @type {RequestOptions}
-         */
-        const options = {headers};
+    https.get(url, options, (response) => {
+      /***
+       * @type {ResponseStream}
+       */
+      let stream;
+      const headers = { 'content-type': response.headers['content-type'] };
 
-        https.get(url, options, (response) => {
-            /***
-             * @type {ResponseStream}
-             */
-            let stream;
-            const headers = {'content-type': response.headers['content-type']};
+      if (
+        response.headers['content-type'].indexOf('html') > -1 ||
+        response.headers['content-type'].indexOf('json') > -1
+      ) {
+        stream = new ResponseStream(ResponseStreamType.BINARY);
+      } else {
+        stream = new ResponseStream(ResponseStreamType.PLAINTEXT);
+      }
 
-            if (response.headers['content-type'].indexOf('html') > -1 || response.headers['content-type'].indexOf('json') > -1) {
-                stream = new ResponseStream(ResponseStreamType.BINARY);
-            } else {
-                stream = new ResponseStream(ResponseStreamType.PLAINTEXT);
-            }
+      response.on('data', (chunk) => {
+        stream.push(chunk);
+      });
 
-            response.on('data', (chunk) => {
-                stream.push(chunk);
-            });
-
-            response.on('end', () => {
-                resolve({
-                    ...stream.dump(),
-                    headers
-                });
-            });
-
-            response.on('error', (err) => {
-                reject(err);
-            })
+      response.on('end', () => {
+        resolve({
+          ...stream.dump(),
+          headers,
         });
-    });
-}
+      });
 
+      response.on('error', (err) => {
+        reject(err);
+      });
+    });
+  });
+}
 
 /***
  *
@@ -56,41 +57,42 @@ async function issueRequest(url, headers) {
  * @returns {Promise<{body: string, statusCode: number}>}
  */
 exports.handler = async (event) => {
-    /**
-     * @type {ResponseStream}
-     */
-    let responseStream;
+  /**
+   * @type {ResponseStream}
+   */
+  let responseStream;
 
-    let url = `https://sampleserver6.arcgisonline.com/${event['pathParameters']['proxy']}`;
-    let httpMethod;
+  let url = `https://sampleserver6.arcgisonline.com/${event['pathParameters']['proxy']}`;
+  let httpMethod;
 
-    console.log('url will be ', url);
+  console.log('url will be ', url);
 
-    if (event.rawQueryString) {
-        url = `${url}?${event['rawQueryString']}`;
-    }
+  if (event.rawQueryString) {
+    url = `${url}?${event['rawQueryString']}`;
+  }
 
-    if (event.httpMethod) {
-        httpMethod = event.httpMethod;
-    } else {
-        httpMethod = event['requestContext']['http']['method'];
-    }
+  if (event.httpMethod) {
+    httpMethod = event.httpMethod;
+  } else {
+    httpMethod = event['requestContext']['http']['method'];
+  }
 
-    let headers = '';
-    if (event['headers']) {
-        headers = event['headers'];
-    }
+  let headers = '';
+  if (event['headers']) {
+    headers = event['headers'];
+  }
 
-    delete headers['Host'];
-    delete headers['host'];
+  delete headers['Host'];
+  delete headers['host'];
 
-    let body = event['body'] || null;
+  let body = event['body'] || null;
 
-    try {
-        return issueRequest(url, headers);
-    } catch (err) {
-        return {
-            statusCode: 500, body: `Content error: ${JSON.stringify(err)}`
-        }
-    }
+  try {
+    return issueRequest(url, headers);
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: `Content error: ${JSON.stringify(err)}`,
+    };
+  }
 };
--- response-stream.js
+++ response-stream.js
@@ -1,60 +1,62 @@
 export class ResponseStreamType {
-    static PLAINTEXT = 'PLAINTEXT';
-    static BINARY = 'BINARY';
+  static PLAINTEXT = 'PLAINTEXT';
+  static BINARY = 'BINARY';
 }
 
 export class ResponseStream {
-    /***
-     * @type { string | Buffer[] }
-     */
-    buffer;
+  /***
+   * @type { string | Buffer[] }
+   */
+  buffer;
 
-    /**
-     * @type {string}
-     */
-    streamType;
+  /**
+   * @type {string}
+   */
+  streamType;
 
-    /***
-     *
-     * @param streamType {string}
-     * @constructor
-     */
-    constructor(streamType) {
-        this.streamType = streamType;
-        this.buffer = [];
+  /***
+   *
+   * @param streamType {string}
+   * @constructor
+   */
+  constructor(streamType) {
+    this.streamType = streamType;
+    this.buffer = [];
+  }
+
+  /***
+   *
+   * @param val {string | Buffer}
+   */
+  push(val) {
+    if (this.streamType === ResponseStreamType.PLAINTEXT) {
+      this.buffer.push(Buffer.from(val).toString('utf-8'));
+    } else {
+      this.buffer.push(val);
     }
+  }
 
-    /***
-     *
-     * @param val {string | Buffer}
-     */
-    push(val) {
-        if (this.streamType === ResponseStreamType.PLAINTEXT) {
-            this.buffer.push(Buffer.from(val).toString('utf-8'));
-        } else {
-            this.buffer.push(val);
-        }
+  /***
+   *
+   * @returns {{responseCode: number, isBase64Encoded: boolean, body: string}}
+   */
+  dump() {
+    if (this.streamType === ResponseStreamType.BINARY) {
+      const body = Buffer.concat(
+        this.buffer.map((item) => Buffer.from(item))
+      ).toString('base64');
+      return {
+        responseCode: 200,
+        isBase64Encoded: true,
+        body,
+      };
+    } else {
+      const body = this.buffer.join('');
+      return {
+        responseCode: 200,
+        isBase64Encoded: false,
+        body,
+      };
     }
-
-    /***
-     *
-     * @returns {{responseCode: number, isBase64Encoded: boolean, body: string}}
-     */
-    dump() {
-        if (this.streamType === ResponseStreamType.BINARY) {
-            const body = Buffer.concat(this.buffer.map(item => Buffer.from(item))).toString('base64');
-            return {
-                responseCode: 200,
-                isBase64Encoded: true,
-                body
-            }
-        } else {
-            const body = this.buffer.join('');
-            return {
-                responseCode: 200,
-                isBase64Encoded: false,
-                body
-            }
-        }
-    }
+  }
 }
